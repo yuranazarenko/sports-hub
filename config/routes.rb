9d3cbeb12723de5c67497b9ab7eab8f23718381b
yuranazarenko <yuranazarenkomail@gmail.com>
2021-09-16 13:40:30 +0300

    [SH-1] Add Bootstrap to project and create a main page skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get "/app", to: "application#index", as: "home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
