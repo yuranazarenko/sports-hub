461b358912db60aaeaa5176f8890c7cb1a1650b0
yuranazarenko <yuranazarenkomail@gmail.com>
2021-09-21 14:05:44 +0300

    [SH-4] Basic Device Auth

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true,
+                       confirmation: true,
+                       length: { within: 1..40 }
+  validates :password, confirmation: true,
+                       length: { within: 1..40 },
+                       allow_blank: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         :registerable,
+         jwt_revocation_strategy: JwtDenylist
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "application#index"
+  devise_for :users,
+             controllers: {
+               sessions: "users/sessions",
+               registrations: "users/registrations"
+             }
   get "/app", to: "application#index", as: "home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
